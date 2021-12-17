76fc6e82a384e50cd7398a1a80bff651b9c786ad
João Vitor Altermann Martinelli <76072847+joaomarti755@users.noreply.github.com>
2021-12-17 15:45:55 -0300

    Contador em react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+export default function App() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [number, setNumber] = useState(0)
+
+    function menos() {
+    setNumber (number -1)
+        console.log(number)
+    }
+    function mais() {
+    setNumber (number +1)
+        console.log(number)
+    }
+    function reset() {
+    setNumber(0)
+    }
+
+    return <div>
+        <h1>Contador</h1>
+        <p>{number}</p>
+        <button onClick={menos}>Menos</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={mais}>Mais</button>
     </div>
-  );
 }
-
-export default App;
